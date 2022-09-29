cd842576b26d4b8c08f7ab9ad0ddc639c216b1d6
aakash894 <113110858+aakash894@users.noreply.github.com>
2022-09-29 18:51:28 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-set -x
-
-git init
-touch test.txt
-echo "Hello" > test.txt
-git status
-git add test.txt
-git commit -m "initial commit"
-
+#action=$1
+#set -x
 function create_branch () {
     branch=$1
     git rev-parse --verify "$branch"
@@ -23,7 +15,7 @@ function merge_branch () {
     merge_branch=$1
     git rev-parse --verify "$merge_branch"
     if [[ $? -eq 0 ]]; then
-        git checkout master
+        git checkout main
         git merge "$merge_branch"
     else 
     echo "$merge_branch"  not exist. Please check your branch name.
@@ -34,7 +26,7 @@ function rebase_branch () {
     rebase_branch=$1
     git rev-parse --verify "$rebase_branch";
     if [[ $? -eq 0 ]]; then
-        git checkout master
+        git checkout main
         git rebase "$rebase_branch"
     else 
     echo "$rebase_branch"  not exist. Please check your branch name.
@@ -45,15 +37,26 @@ function delete_name () {
      dlt_branch=$1
     git rev-parse --verify "$dlt_branch";
     if [[ $? -eq 0 ]]; then
-    git checkout master	
     git branch -d "$dlt_branch"
     else
     echo "$dlt_branch" does not exist. Please check youtr branch name.
     fi
 }
 
+
+function push_branch () {
+    push_branch=$1
+    git rev-parse --verify "$push_branch";
+    if [[ $? -eq 0 ]]; then
+    git push origin "$push_branch"
+    else
+    echo "Branch is not exist.Please check your branch name"
+    fi
+}
+
 action=$1
 branch=$2
+
 case $action in
         create)
             branch_name=$branch
@@ -74,4 +77,8 @@ case $action in
             dlt_branch_name=$branch
             delete_name "$dlt_branch_name"
         ;;
-esac
+        push)
+            push=$branch
+            push_branch "$push"
+        ;;
+        esac
